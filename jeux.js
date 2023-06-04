568dd404679ba1fae2f5906aba5e92fe19e3d34d
Romane111 <135544536+Romane111@users.noreply.github.com>
2023-06-04 20:24:56 +0200

    Add files via upload

---
+++ jeux.js
@@ -0,0 +1,48 @@
+// decor
+//les fonctions qui permettent de reduire les Caractéristiques vitales 
+
+function sante{
+
+}
+
+function hydratation{
+
+}
+
+function oxygene{
+
+}
+
+//fonction celle a droite qui calcule le temps du jeu, en secondes, multiplié par le nombre de
+//machines de terraformation (foreuse, radiateurs, plantations)
+
+function temps{
+
+}
+
+//fonction qui fait apparaitre le bouton boire
+
+var buttton = document.querySelector(boire)
+function boire{
+
+}
+
+//fonction qui fait apparaitre le bouton manger
+
+var buttton = document.querySelector(manger)
+function manger{
+	
+}
+
+
+// Apparait le bouton de recharge
+
+var button= document.querySelector(bas)
+
+function apparait{
+	while // tant que l'habitat n'est pas la on fait rien sinon le bouton recharge apparait: display: .
+}
+
+//chapitre 1
+
+//Fait apparaitre les ressources de maniere random
